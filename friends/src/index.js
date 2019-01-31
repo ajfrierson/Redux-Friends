f9caab87f173a3c6a6c0cff0950efed31de330c1
ajfrierson <alvinfrierson@fuller.edu>
2019-01-31 14:23:54 -0800

    added Router, Provider, and createStore to src/index.js

--- friends/src/index.js
+++ friends/src/index.js
@@ -12,12 +12,22 @@ import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(logger, thunk)
+));
 
 
+ReactDOM.render(
+<Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+</Provider>
+,document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
