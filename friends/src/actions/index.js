01f8ddcf73ab1c2825d2ef28d421d5dec8f63c3b
ajfrierson <alvinfrierson@fuller.edu>
2019-01-31 14:43:29 -0800

    added a get request to actions

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -5,3 +5,18 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 export const ERROR = 'ERROR';
 
 //GET
+export const getFriends = () => {
+    return (dispatch) => {
+      dispatch({ type: LOADING});
+      
+      axios
+      .get('http://localhost:5000/api/friends')
+      .then(response => {
+          dispatch({ type: GET_FRIENDS, friends: response.data })
+      })
+      .catch(err => {
+          dispatch({ type: ERROR, errorMessage: 'Error fetching the friends data.'})
+      })
+    }
+}
+
